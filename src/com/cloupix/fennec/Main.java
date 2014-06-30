f0d61e656b57f7e4f83f6dafaed3170ddbe1f7cf
AlonsoApp <AlonsoApp@gmail.com>
2014-06-30 09:32:33 +0200

    First commit

---
+++ src/com/cloupix/fennec/Main.java
@@ -0,0 +1,12 @@
+package com.cloupix.fennec;
+
+/**
+ * Created by AlonsoUSA on 30/06/14.
+ *
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.print(":D");
+    }
+}
